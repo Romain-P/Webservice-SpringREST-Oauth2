dffd85bfc02f26ae07724dc8f69d1898768adcdd
Romain-p <romain.pillot@epitech.eu>
2017-09-15 11:25:46 +0200

    added some logic to stat service, some calculations to do remain

--- src/main/java/com/ortec/gta/service/StatService.java
+++ src/main/java/com/ortec/gta/service/StatService.java
@@ -1,8 +1,20 @@
 package com.ortec.gta.service;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Sets;
+import com.ortec.gta.database.ActivityRepositoryImpl;
+import com.ortec.gta.database.UserRepositoryImpl;
+import com.ortec.gta.database.model.dto.ActivityDTO;
 import com.ortec.gta.database.model.dto.StatDTO;
+import com.ortec.gta.database.model.dto.UserDTO;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * @Author: romain.pillot
@@ -11,13 +23,85 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class StatService {
+    @Autowired
+    private UserRepositoryImpl userRepository;
+
+    @Autowired
+    private ActivityRepositoryImpl activityRepository;
+
+    private final Cache<String, Set> listCache;
+    private final Cache<String, Long> statCache;
+
+    public StatService() {
+        this.listCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(1, TimeUnit.DAYS)
+                .build();
+
+        this.statCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(1, TimeUnit.DAYS)
+                .build();
+    }
+
     public StatDTO getStatistics() {
         StatDTO.ActivityStatDTO activity = new StatDTO.ActivityStatDTO("GTA", 10);
 
-        return new StatDTO(1000,
-                1000,
-                1000,
-                Sets.newHashSet(activity),
-                Sets.newHashSet(activity));
+        Set<UserDTO> users = getUsers();
+        Set<ActivityDTO> activities = getParentActivities();
+
+        return new StatDTO(users.size(),
+                weekValids(users),
+                monthValids(users),
+                weekTop(activities),
+                monthTop(activities));
+    }
+
+    @SneakyThrows
+    @SuppressWarnings("unchecked")
+    private Set<StatDTO.ActivityStatDTO> weekTop(Set<ActivityDTO> activities) {
+        return listCache.get("weekTop", () -> activities.stream()
+                .map(x -> {
+                    long manDays = 0;
+                    //TODO: calculate subactivities weeks mandays, the last week
+                    return new StatDTO.ActivityStatDTO(x.getName(), manDays);
+                }).collect(Collectors.toSet()));
+    }
+
+    @SneakyThrows
+    @SuppressWarnings("unchecked")
+    private Set<StatDTO.ActivityStatDTO> monthTop(Set<ActivityDTO> activities) {
+        return listCache.get("weekTop", () -> activities.stream()
+                .map(x -> {
+                    long manDays = 0;
+                    //TODO: calculate subactivities weeks mandays, the last month
+                    return new StatDTO.ActivityStatDTO(x.getName(), manDays);
+                }).collect(Collectors.toSet()));
+    }
+
+    @SneakyThrows
+    private long weekValids(Set<UserDTO> users) {
+        return statCache.get("week", () -> users.stream().filter(x -> {
+            //TODO: users who pointed to 100% their week
+            return true;
+        }).count());
+    }
+
+    @SneakyThrows
+    private long monthValids(Set<UserDTO> users) {
+        return statCache.get("month", () -> users.stream().filter(x -> {
+            //TODO: users who pointed to 100% their month
+            return true;
+        }).count());
+    }
+
+    @SneakyThrows
+    @SuppressWarnings("unchecked")
+    private Set<UserDTO> getUsers() {
+        return listCache.get("users", () -> userRepository.findAll());
+    }
+
+    @SneakyThrows
+    @SuppressWarnings("unchecked")
+    private Set<ActivityDTO> getParentActivities() {
+        return listCache.get("activities", () -> activityRepository.findParentActivities());
     }
 }
